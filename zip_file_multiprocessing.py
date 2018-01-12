c6b9e01d7f700c63f08045bedb702c4de832f8eb
adwardlee <4065156@qq.com>
2018-01-12 16:06:00 +0800

    Create zip_file_multiprocessing.py

---
+++ zip_file_multiprocessing.py
@@ -0,0 +1,55 @@
+# importing required modules
+from zipfile import ZipFile
+import os
+from multiprocessing import Pool
+
+split_num = 1000
+ 
+def get_all_file_paths(directory):
+ 
+    # initializing empty file paths list
+    file_paths = []
+ 
+    # crawling through directory and subdirectories
+    for root, directories, files in os.walk(directory):
+        for filename in files:
+            # join the two strings in order to form the full filepath.
+            filepath = os.path.join(root, filename)
+            file_paths.append(filepath)
+ 
+    # returning all file paths
+    return file_paths        
+def zip_worker(names):
+    with ZipFile(names[1],'w') as zip:
+        for file in names[0]:
+            zip.write(file)
+ 
+def main():
+    # path to folder which needs to be zipped
+    directory = './python_files'
+ 
+    # calling function to get all file paths in the directory
+    file_paths = get_all_file_paths(directory)
+ 
+    # printing the list of all files to be zipped
+    #print('Following files will be zipped:')
+    #for file_name in file_paths:
+        #print(file_name)
+    file_name1 = [[] for x in xrange(int(len(file_name)/split_num))]
+    zipnames = [ str(x) + 'zipfile.zip' for x in xrange(int(len(file_name)/split_num))]
+    union = (file_name1,zipnames)
+    function_input = ((a,b) for a,b in union)
+    for i in xrange(len(file_name)):
+        k = i / split_num
+        file_name1[k].append(file_name[i])
+    pool.map(zip_worker, function_input)
+    # writing files to a zipfile
+    with ZipFile('my_python_files.zip','w') as zip:
+        # writing each file one by one
+        for file in file_paths:
+            zip.write(file)
+ 
+    print('All files zipped successfully!')        
+ 
+if __name__ == "__main__":
+    main()
